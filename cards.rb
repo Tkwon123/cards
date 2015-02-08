04683f85e4210ea5a1bee64f6133752f5bae69bb
Tkwon123 <tkw21189@gmail.com>
2015-02-08 12:00:04 -0500

    added deck class

--- cards.rb
+++ cards.rb
@@ -1,9 +1,43 @@
-class Deck
 
+class Deck
+	def initialize
+		@ID = *(1..52)
+		@suits = ["Spade", "Heart", "Diamond", "Club"]
+		@ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+		@cards = []
+	end
+
+	def cards
+		@cards.each do |card|
+			puts card.card
+		end
+	end
+
+	def shuffle
+		@cards.shuffle
+	end
+
+	def create
+		puts "Filling with the following suits:"
+		puts "		#{@suits}"
+		puts "Filling with the following ranks:"
+		puts "		#{@ranks}"
+		@suits.each do |suit|
+			@ranks.each do |rank|
+				@cards<<Card.new(:rank => rank, :suit => suit)
+			end
+		end
+		puts "***Deck complete!****"
+	end
+
+	def method_missing (method_name)
+		puts "there's no method called '#{method_name}'"
+	end
 end
 
 
 class Card
+
 	attr_reader :rank, :suit, :ID
 
 	def initialize (args)
@@ -25,13 +59,8 @@ class Card
 	end
 end
 
-@ID = *(1..52)
-@suits = ["Spade", "Heart", "Diamond", "Club"]
-@ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-@deck = []
-
-@suits.each do |suit|
-	@ranks.each do |rank|
-		@deck<<Card.new(:rank => rank, :suit => suit)
-	end
-end
+a = Deck.new
+a.create
+a.cards
+a.shuffle
+a.cards

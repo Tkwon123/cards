e954532c3a5c7a44ca703080ae59eb8cc9e0082c
Tkwon123 <tkw21189@gmail.com>
2015-02-10 22:51:06 -0500

    adding some actions to the deck

--- cards.rb
+++ cards.rb
@@ -1,5 +1,5 @@
-
 class Deck
+
 	def initialize
 		@ID = *(1..52)
 		@suits = ["Spade", "Heart", "Diamond", "Club"]
@@ -7,16 +7,19 @@ class Deck
 		@cards = []
 	end
 
+	#Print out all the cards in the deck
 	def cards
 		@cards.each do |card|
 			puts card.card
 		end
 	end
 
+	#Move the cards around
 	def shuffle
 		@cards.shuffle
 	end
 
+	#Make the deck by looping through suits and ranks
 	def create
 		puts "Filling with the following suits:"
 		puts "		#{@suits}"
@@ -30,6 +33,16 @@ class Deck
 		puts "***Deck complete!****"
 	end
 
+	def deal
+		@cards.pop
+	end
+
+	#Count of current cards herea = Deck.new
+	def count
+		puts "Deck currently holds #{@cards.count}"
+	end
+
+	#Error handling for missing methods
 	def method_missing (method_name)
 		puts "there's no method called '#{method_name}'"
 	end
@@ -46,21 +59,48 @@ class Card
 		@ID =  args[:ID]
 	end
 
+	#Read a card's values
 	def card
 		puts "#{@card} #{@suit}"
 	end
 
+	#Just the suit
 	def suit
 		puts "#{@suit}"
 	end
 
+	#Just the value
 	def value
 		put "#{card}"
 	end
 end
 
+class Deal
+	#Deal out cards
+
+	#hand card from the top of the deck
+
+	#reduce the number of cards in the deck
+
+	#replace 
+
+end
+
+#Throwing down this interface for now
+puts "What would you like to do?"
+puts "1. Create Deck"
+puts "2. Count cards in the deck"
+
+response = gets.chomp.to_i
 a = Deck.new
-a.create
-a.cards
-a.shuffle
-a.cards
+
+case response
+
+when 1
+	a.create
+	a.cards
+when 2
+	a.count
+when 3
+	a.deal
+end
